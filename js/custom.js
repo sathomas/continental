856957dd6e936d092278ea3833de3ecdc613db81
Stephen Thomas <stephen@sathomas.me>
2013-04-29 17:19:17 -0400

    fixed mobile imagery and glyph size calculation

--- js/custom.js
+++ js/custom.js
@@ -3,8 +3,20 @@ window.onload = function() {
 
    var sizeMaps = function() {
      var w = Math.min(1000,$(window).width());
+     $(".hero h1")
+       .css("padding-left", (w*0.015)+"px")
+       .css("font-size",    (w*0.12)+"px")
+       .css("line-height",  (w*0.11)+"px");
+     $(".hero h2")
+       .css("top",          (w*0.11)+"px")
+       .css("padding-left", (w*0.02)+"px")
+       .css("font-size",    (w*0.059)+"px")
+       .css("line-height",  (w*0.059)+"px");
      if (w > 768) { w = w/3;} else { w = 333;}
-     $(".map-stack").css("font-size", (w-31)+"px").css("width",(w-31)+"px").css("height",(w-31)+"px");
+     $(".map-stack")
+       .css("font-size", (w-31)+"px")
+       .css("width",     (w-31)+"px")
+       .css("height",    (w-31)+"px");
      $(".legend").css("width", (w-31)+"px");
      if ($("#example-maps .large-8").offset().left === $("#example-maps .large-4").offset().left) {
        $("#example-maps .large-4").height("auto");
@@ -13,7 +25,10 @@ window.onload = function() {
        $("#example-maps .large-4").height($("#example-maps .large-8").height());
        $("#example-maps .large-4 figure").last().css("position","absolute").css("bottom","0");
      }
+     w = $(".bullet-item.glyph").width();
+     $(".bullet-item.glyph").css("font-size",w+"px").css("line-height",w+"px");
    };
+window.test = sizeMaps;
    sizeMaps();
    $(window).resize(sizeMaps);
 
